b63afbcfb88611f988e39c1868041c9aa58daac2
Jan-Christoph Meier <jan@codejunky.org>
2017-04-17 13:43:21 +0200

    Fix printf

--- util/led_matrix_converter.c
+++ util/led_matrix_converter.c
@@ -3,7 +3,11 @@
 #include <stdint.h>
 
 const uint64_t IMAGES[] = {
-  0x0101010101010101
+  0xc100140000000003,
+  0x0000040208000000,
+  0x0000380808000000,
+  0x0000701010000000,
+  0x0000e02020000000
 };
 
 const int IMAGES_LEN = sizeof(IMAGES)/8;
@@ -11,11 +15,10 @@ const int IMAGES_LEN = sizeof(IMAGES)/8;
 int main() {
   printf("animation = {\n");
   for(int i=0; i < IMAGES_LEN; i++) {
-    printf("{ ");
     uint64_t image = IMAGES[i];
-    for(int j=0; j<8; j++) {
+    for(int j=0; j<8; j++) { 
       char result = (image >> (j * 8)) & 0xFF;
-      printf("0x%02x,", result);  
+      printf("0x%hhx, ", result);  
     }
     printf("}, \n");
   }
