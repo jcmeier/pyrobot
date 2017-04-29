519fb8206b580b0a31748af1a78538665b6bf884
Jan-Christoph Meier <jan@codejunky.org>
2017-04-29 16:38:42 +0200

    New animation

--- firmware/display.lua
+++ firmware/display.lua
@@ -1,4 +1,4 @@
-require("displayfont")
+
 MAX7219_REG_NOOP        = 0x00
 MAX7219_REG_DECODEMODE  = 0x09
 MAX7219_REG_INTENSITY   = 0x0A
@@ -6,32 +6,10 @@ MAX7219_REG_SCANLIMIT   = 0x0B
 MAX7219_REG_SHUTDOWN    = 0x0C
 MAX7219_REG_DISPLAYTEST = 0x0F
 
-all =   {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
-happy = {0x3C, 0x42, 0xA5, 0x81, 0xA5, 0x99, 0x42, 0x3C}
-frown = {0x3C, 0x42, 0xA5, 0x81, 0xBD, 0x81, 0x42, 0x3C}
-sad = {0x3C, 0x42, 0xA5, 0x81, 0x99, 0xA5, 0x42, 0x3C }
-
-faces = {all, happy, frown, sad}
 
 function sendByte(reg, data)
   spi.send(1,reg * 256 + data)
   tmr.delay(50)
-end
-
-function displayFace(faceIndex)
-  local face = faces[faceIndex]
-  -- iterates over all 8 columns and sets their values
-  for i=1,8 do
-    sendByte(i,face[i])
-  end
-end
-
-function display_letter(letter)
-    for i=1,8 do
-        if letters[letter] then
-            sendByte(i,letters[letter][i])
-        end
-    end
 end
 
 function show_animation_from_file(position)
@@ -40,12 +18,14 @@ function show_animation_from_file(position)
         if s.size == position then
             position = 0
         end    
-        file.seek(position)
-        for i=1,8 do
-            position = position + 1
+        file.seek("set", position)
+        for i=0,7 do
             file.seek("set", position)
-            sendByte(i, file.read(1))
+            position = position + 1
+            data = file.read(1) 
+            sendByte(i+1, string.byte(data))   
         end    
+        
         file.close()
         tmr.alarm(0, 2000, 0, function() 
                 show_animation_from_file(position)
@@ -66,24 +46,5 @@ function setup_display()
     tmr.stop(0)
 end
 
-stop_moody = false
+setup_display()
 
-function moody(i)
-    if not stop_moody then
-        faceIndex = (i % 4) + 1
-        displayFace(faceIndex)
-        tmr.alarm(0, 2000, 0, function()
-            moody(faceIndex)
-        end);
-    end
-end
-
-function display_message(message, index)
-    index = (index % string.len(message)) + 1
-    stop_moody = true
-    char = string.sub(message, index, index)
-    display_letter(string.upper(char))
-    tmr.alarm(0, 2000, 0, function()
-        display_message(message, index)
-    end);
-end
