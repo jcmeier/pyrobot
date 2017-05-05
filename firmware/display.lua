c7821fd062cf444efce2bbf51c84f0f4d416f109
Jan-Christoph Meier <j.meier@mytaxi.com>
2017-05-05 17:06:55 +0200

    Stabilize animations

--- firmware/display.lua
+++ firmware/display.lua
@@ -5,34 +5,40 @@ MAX7219_REG_INTENSITY   = 0x0A
 MAX7219_REG_SCANLIMIT   = 0x0B
 MAX7219_REG_SHUTDOWN    = 0x0C
 MAX7219_REG_DISPLAYTEST = 0x0F
-
+ 
 
 function sendByte(reg, data)
   spi.send(1,reg * 256 + data)
   tmr.delay(50)
 end
 
-function show_animation_from_file(position)
-    if file.open("animation.bytes", "r") then
-        s = file.stat("animation.bytes")
-        if s.size == position then
-            position = 0
+function show_animation_from_file(position, size)
+    fd = file.open("animation.bytes", "r")
+    if fd then
+       
+        if size == position then
+             position = 0
         end    
-        file.seek("set", position)
+        
         for i=0,7 do
-            file.seek("set", position)
+            fd:seek("set", position)
             position = position + 1
-            data = file.read(1) 
+            data = fd:read(1) 
             sendByte(i+1, string.byte(data))   
         end    
         
-        file.close()
+        fd:close()
         local mytmr = tmr.create()
-        mytmr:alarm(0, 500, 0, function() 
-                show_animation_from_file(position)
-            end)
+        mytmr:alarm(500, tmr.ALARM_SINGLE, function() 
+            show_animation_from_file(position, size)
+        end)
     end
 end        
+
+function start_animation()
+    s = file.stat("animation.bytes")
+    show_animation_from_file(0, s.size)
+end
 
 function setup_display()
     spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, 16, 8)
