d622c5e58faa0508302013b632677d0f20b85ee3
Jan-Christoph Meier <j.meier@mytaxi.com>
2017-05-22 16:26:56 +0200

    Set time from internet

--- display/settime.lua
+++ display/settime.lua
@@ -1,10 +1,41 @@
 ds3231 = require("ds3231")
-ds3231.init(3, 4)
---ds3231.setTime(30, 5, 20, 1, 7, 5, 17);
+ds3231.init(3, 1)
 
--- Get date and time
-second, minute, hour, day, date, month, year = ds3231.getTime();
+function sync_time()
 
--- Print date and time
-print(string.format("Time & Date: %s:%s:%s %s/%s/%s", 
-    hour, minute, second, date, month, year))
+    sntp.sync(nil,
+      function(sec, usec, server, info)
+        tm = rtctime.epoch2cal(sec)
+        sec = tonumber(tm["sec"])
+        min = tonumber(tm["min"])
+        hour = tonumber(tm["hour"]) + 2
+        day = tonumber(tm["day"])
+        month = tonumber(tm["mon"])
+        year = tonumber(tm["year"])
+        
+     -- (second, minute, hour, day, date, month, year, disOsc
+        ds3231.setTime(sec, min, hour, 2, day, month, 17);
+        
+        print(string.format("%04d/%02d/%02d %02d:%02d:%02d", 
+        tm["year"], tm["mon"], tm["day"], tm["hour"], tm["min"], tm["sec"]))
+    
+      end,
+      function()
+       print('failed!')
+      end
+    )
+end
+
+
+function wifi_connect()
+    station_cfg={}
+    station_cfg.ssid="WIFI"
+    station_cfg.pwd="PASSWORD"
+    wifi.setmode(wifi.STATION)
+    wifi.sta.config(station_cfg)
+    --wifi.sta.sethostname("Gamepad of death")
+    wifi.sta.connect()
+    
+end
+
+wifi_connect()
